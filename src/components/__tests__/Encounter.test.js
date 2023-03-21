59f7bbe06379936f68a62ea7aba41b5562344b82
Hayley Ashby <hayleyashby7@gmail.com>
2023-03-21 10:41:19 +0000

    Implemented file structure to allow for organisation as codebase grows

--- src/components/__tests__/Encounter.test.js
+++ src/components/__tests__/Encounter.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Encounter from './Encounter';
+import Encounter from '../Encounter';
 
 describe('Encounter has correct header and content', () => {
 	it('renders Encounter heading', () => {
