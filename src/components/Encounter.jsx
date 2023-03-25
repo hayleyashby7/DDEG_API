a8152804b20ee5a55be8a87caba20befdc4396b1
Hayley Ashby <hayleyashby7@gmail.com>
2023-03-25 16:50:11 +0000

    Initial implementation of Encounter returning data from API call

--- src/components/Encounter.jsx
+++ src/components/Encounter.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { getMonstersFromAPI } from './utilities/API';
 
-const Encounter = () => {
-	return (
-		<div>
-			<h2>Encounter</h2>
-		</div>
+const Encounter = ({ challengeRating = 3 }) => {
+	const [monsters, setMonsters] = useState([]);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await getMonstersFromAPI(challengeRating);
+				if (!ignore) {
+					if (data.count > 0) {
+						setMonsters(data.results);
+					} else {
+						setError(true);
+					}
+				}
+			} catch (err) {
+				setError(true);
+			}
+		};
+
+		let ignore = false;
+		fetchData();
+		return () => {
+			ignore = true;
+		};
+	}, [challengeRating]);
+
+	const hasData = (data) => {
+		if (data === undefined) return false;
+		else return true;
+	};
+
+	return error ? (
+		<p>Unable to retrieve data</p>
+	) : (
+		<ul>
+			{monsters.map((monster) => (
+				<li key={monster.slug}>{monster.name}</li>
+			))}
+		</ul>
 	);
 };
 
