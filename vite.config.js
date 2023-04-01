86c05c9d07b63e82a2f343bc718c990f629c4dab
Hayley Ashby <hayleyashby7@gmail.com>
2023-04-01 16:28:32 +0100

    Configure ESLint and Prettier with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), eslint()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
