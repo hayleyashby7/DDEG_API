082c3a50381d9532c0e1be92905751fe6593dded
Hayley Ashby <hayleyashby7@gmail.com>
2023-04-01 16:28:57 +0100

    Fix errors in Encounter found on linter

--- src/components/Encounter.jsx
+++ src/components/Encounter.jsx
@@ -1,43 +1,48 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { getMonstersFromAPI } from './utilities/API';
 
-const Encounter = ({ challengeRating = 3 }) => {
-	const [monsters, setMonsters] = useState([]);
-	const [error, setError] = useState(null);
+function Encounter({ challengeRating = 3 }) {
+    const [monsters, setMonsters] = useState([]);
+    const [error, setError] = useState(null);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const data = await getMonstersFromAPI(challengeRating);
-				if (!ignore) {
-					if (data.count > 0) {
-						setMonsters(data.results);
-					} else {
-						setError(true);
-					}
-				}
-			} catch (err) {
-				setError(true);
-			}
-		};
+    useEffect(() => {
+        let ignore = false;
 
-		let ignore = false;
-		fetchData();
-		return () => {
-			ignore = true;
-		};
-	}, [challengeRating]);
+        const fetchData = async () => {
+            try {
+                const data = await getMonstersFromAPI(challengeRating);
+                if (!ignore) {
+                    if (data.count > 0) {
+                        setMonsters(data.results);
+                    } else {
+                        setError(true);
+                    }
+                }
+            } catch (err) {
+                setError(true);
+            }
+        };
 
-	return error ? (
-		<p>Unable to retrieve data</p>
-	) : (
-		<ul>
-			{monsters.map((monster) => (
-				<li key={monster.slug}>{monster.name}</li>
-			))}
-		</ul>
-	);
-};
+        fetchData();
+
+        return () => {
+            ignore = true;
+        };
+    }, [challengeRating]);
+
+    return error ? (
+        <p>Unable to retrieve data</p>
+    ) : (
+        <ul>
+            {monsters.map((monster) => (
+                <li key={monster.slug}>{monster.name}</li>
+            ))}
+        </ul>
+    );
+}
+
+Encounter.propTypes = { challengeRating: PropTypes.number };
+Encounter.defaultProps = { challengeRating: 3 };
 
 export default Encounter;
