095e0d4bc63004ec57ca5065d8c245230fa8aca9
Hayley Ashby <hayleyashby7@gmail.com>
2023-04-04 08:43:49 +0100

    Test for handling when API call fails

--- src/components/__tests__/Encounter.test.jsx
+++ src/components/__tests__/Encounter.test.jsx
@@ -28,16 +28,35 @@ describe('Encounter', () => {
         render(<Encounter />);
 
         // Assert
-
         // Heading is displayed
-        expect(screen.getByRole('heading')).toHaveTextContent('List of Monsters');
+        expect(screen.getByRole('heading')).toHaveTextContent(/List of Monsters/i);
 
         // Mocked data is displayed
-        expect(await screen.findByText('Goblin')).toBeInTheDocument();
-        expect(await screen.findByText('Orc')).toBeInTheDocument();
+        expect(await screen.findByText(/Goblin/i)).toBeInTheDocument();
+        expect(await screen.findByText(/Orc/i)).toBeInTheDocument();
 
         // No error message is displayed
-        expect(screen.queryByText('No monsters found')).not.toBeInTheDocument();
+        expect(screen.queryByText(/No monsters found/i)).not.toBeInTheDocument();
     });
 
+    it('should show an error message with a failed API call', async () => {
+        // Arrange
+
+        // Mock the API call
+        getMonstersFromAPI.mockReturnValue(Promise.reject(new Error('API call failed')));
+
+        // Act
+        render(<Encounter />);
+
+        // Assert
+        // Heading is still displayed
+        expect(screen.getByRole('heading')).toHaveTextContent(/List of Monsters/i);
+
+        // Error message is displayed
+        expect(await screen.findByText(/No monsters found/i)).toBeInTheDocument();
+
+        // Mocked data is not displayed
+        expect(screen.queryByText(/Goblin/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Orc/i)).not.toBeInTheDocument();
+    });
 });
