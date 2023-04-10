65c010ee826139fe30b2786a9cecc8c2de52ffd8
Hayley Ashby <hayleyashby7@gmail.com>
2023-04-10 12:33:12 +0100

    Ensure that expanding section expand/collapse correctly and only on Expand element click

--- src/components/__tests__/Row.test.jsx
+++ src/components/__tests__/Row.test.jsx
@@ -55,7 +55,7 @@ describe('Row', () => {
             container: document.body.appendChild(tbody),
             baseElement: document.body,
         });
-        await user.click(screen.getByText(/Goblin/i));
+        await user.click(screen.getByTitle(/Expand/i));
 
         // Assert
         // Basic row data should be rendered
@@ -69,5 +69,52 @@ describe('Row', () => {
         expect(
             screen.getByRole('cell', { name: /Goblin 1 Humanoid Small Neutral/i }),
         ).toBeInTheDocument();
+    });
+
+    it('should remove expanded row when clicked again', async () => {
+        // Arrange
+        const rowKey = 'goblin';
+        const data = {
+            name: 'Goblin',
+            challenge_rating: '1',
+            type: 'Humanoid',
+            size: 'Small',
+            alignment: 'Neutral',
+        };
+        const user = userEvent.setup();
+        const tbody = document.createElement('tbody');
+
+        // Act
+        render(<Row rowKey={rowKey} data={data} />, {
+            container: document.body.appendChild(tbody),
+            baseElement: document.body,
+        });
+        await user.click(screen.getByTitle(/Expand/i));
+
+        //  Check data after first click to expand
+        expect(screen.getByText('Goblin', { selector: 'td' })).toBeInTheDocument();
+        expect(screen.getByText('1', { selector: 'td' })).toBeInTheDocument();
+        expect(screen.getByText('Humanoid', { selector: 'td' })).toBeInTheDocument();
+        expect(screen.getByText('Small', { selector: 'td' })).toBeInTheDocument();
+        expect(screen.getByText('Neutral', { selector: 'td' })).toBeInTheDocument();
+        expect(
+            screen.getByRole('cell', { name: /Goblin 1 Humanoid Small Neutral/i }),
+        ).toBeInTheDocument();
+
+        // Click again to collapse
+        await user.click(screen.getByTitle(/Expand/i));
+
+        // Assert
+        // Basic row data should be rendered
+        expect(screen.getByText('Goblin', { selector: 'td' })).toBeInTheDocument();
+        expect(screen.getByText('1', { selector: 'td' })).toBeInTheDocument();
+        expect(screen.getByText('Humanoid', { selector: 'td' })).toBeInTheDocument();
+        expect(screen.getByText('Small', { selector: 'td' })).toBeInTheDocument();
+        expect(screen.getByText('Neutral', { selector: 'td' })).toBeInTheDocument();
+
+        // Exanded row should not be rendered
+        expect(
+            screen.queryByRole('cell', { name: /Goblin 1 Humanoid Small Neutral/i }),
+        ).not.toBeInTheDocument();
     });
 });
