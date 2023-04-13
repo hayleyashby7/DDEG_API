3249929ac287b9120d818ca5d624a8e09fd9cee0
Hayley Ashby <hayleyashby7@gmail.com>
2023-04-13 14:57:14 +0100

    Minor styling adjustments to Button and Party components with updated tests

--- src/components/Form/Button.jsx
+++ src/components/Form/Button.jsx
@@ -8,7 +8,7 @@ function Button({ label, type, ...rest }) {
         <button
             onClick={rest.onClick}
             type={type}
-            className='rounded-lg border-2 border-solid border-orange-100 bg-red-950 p-2 text-orange-100 hover:bg-red-900 hover:text-orange-200 focus:outline-none focus:ring-2 focus:ring-red-900/50'
+            className="rounded-lg border-2 border-solid border-orange-100 bg-red-950 p-2 font-['Alegreya_Sans'] text-orange-100 hover:bg-red-900 hover:text-orange-200 focus:outline-none focus:ring-2 focus:ring-red-900/50"
             {...rest}
         >
             {label}
