b03b41cddb46d5ebdc6f54de1f1f2ea94b72df64
Hayley Ashby <hayleyashby7@gmail.com>
2023-04-13 12:17:46 +0100

    Added fonts for styling and assets folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: {
         relative: true,
         files: ['./index.html', './src/**/*.{js,jsx}', './src/*.{js,jsx}'],
     },
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                dragon: "url('/src/assets/dragon.png')",
+            },
+            fontFamily: {
+                sans: ['Alegreya Sans', 'Alegreya Sans SC', ...defaultTheme.fontFamily.sans],
+                serif: ['Fraunces', 'Libre Baskerville', ...defaultTheme.fontFamily.serif],
+                cursive: ['Goblin One'],
+            },
+        },
     },
     plugins: ['@tailwindcss/forms'],
 };
