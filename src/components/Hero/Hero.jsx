f6ccfbad9030056744fde07ee7663ee3908cc9ee
Hayley Ashby <hayleyashby7@gmail.com>
2023-04-14 20:47:40 +0100

    Changed Hero section to add scrolling effect and sticky header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,64 +1,61 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import Button from '../Form/Button';
+import React, { useEffect, useRef } from 'react';
 import PaperScroll from '../PaperScroll/PaperScroll';
 
-function Hero({ clickCB }) {
-    const [isMinimised, setIsMinimised] = useState(false);
+function Hero() {
+    const heroContent = useRef(null);
+    const heroHeader = useRef(null);
 
-    const heroClick = () => {
-        setIsMinimised(true);
-        clickCB();
+    const handleScroll = () => {
+        const header = heroHeader.current;
+        const content = heroContent.current;
+
+        if (header && content) {
+            const { top } = content.getBoundingClientRect();
+            if (top < 0) {
+                header.classList.add('bg-red-950');
+            } else {
+                header.classList.remove('bg-red-950');
+            }
+        }
     };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
     return (
         <section
-            className={`${
-                isMinimised ? 'h-[20vh] ' : 'h-screen'
-                // eslint-disable-next-line prettier/prettier
-            }  bg-dragon bg-cover bg-top bg-no-repeat`}
+            className='relative h-screen bg-dragon bg-cover bg-top bg-no-repeat'
+            data-testid='hero-main'
         >
-            <div
-                className={`${
-                    isMinimised ? 'h-[20vh]' : 'h-screen '
-                }  flex flex-col bg-neutral-900/50`}
-            >
-                <header className="flex flex-none flex-col text-center font-['MedievalSharp']">
+            <div className='flex h-screen flex-col bg-neutral-900/50'>
+                <header
+                    ref={heroHeader}
+                    className="sticky top-0 z-[999] flex flex-none flex-col py-5 text-center font-['MedievalSharp']"
+                >
                     <h1 className='text-4xl md:text-8xl'>DDEG</h1>
                     <h2 className='text-lg md:text-2xl'>Dungeons & Dragons Encounter Generator</h2>
                 </header>
 
-                {!isMinimised && (
-                    <div className='flex flex-1 grow flex-col justify-evenly text-center'>
-                        <PaperScroll
-                            contents={
-                                <>
-                                    <h2 className="font-['Alegreya_Sans_SC'] text-2xl lg:text-5xl">
-                                        Take the stress out of being a DM
-                                    </h2>
-                                    <p className=" font-['Alegreya_Sans'] text-xl lg:text-3xl">
-                                        Generate an encounter customised to YOUR party
-                                    </p>
-                                </>
-                            }
-                        />
-
-                        <span>
-                            <Button
-                                type='button'
-                                label='Build Your Encounter'
-                                onClick={heroClick}
-                            />
-                        </span>
-                    </div>
-                )}
+                <div className='flex flex-1 flex-col justify-evenly text-center' ref={heroContent}>
+                    <PaperScroll
+                        contents={
+                            <>
+                                <h2 className="font-['Alegreya_Sans_SC'] text-2xl lg:text-5xl">
+                                    Take the stress out of being a DM
+                                </h2>
+                                <p className=" font-['Alegreya_Sans'] text-xl lg:text-3xl">
+                                    Generate an encounter customised to YOUR party
+                                </p>
+                            </>
+                        }
+                    />
+                    <span>Build Your Encounter</span>
+                </div>
             </div>
         </section>
     );
 }
-
-Hero.propTypes = {
-    clickCB: PropTypes.func.isRequired,
-};
 
 export default Hero;
--- src/components/Hero/Hero.test.jsx
+++ src/components/Hero/Hero.test.jsx
@@ -1,54 +1,14 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import Hero from './Hero';
 
 describe('Hero component', () => {
-    beforeEach(() => {
-        vi.clearAllMocks();
-    });
     it('renders the component', () => {
         // Arrange
-        const mockClickCB = vi.fn();
-        const { container } = render(<Hero clickCB={mockClickCB} />);
+        const { container } = render(<Hero />);
 
         // Assert
         expect(container).toMatchSnapshot();
-    });
-
-    it('should call the clickCB function when the button is clicked', async () => {
-        // Arrange
-        const user = userEvent.setup();
-        const mockClickCB = vi.fn();
-
-        // Act
-        render(<Hero clickCB={mockClickCB} />);
-
-        await user.click(screen.getByRole('button', { name: 'Build Your Encounter' }));
-
-        // Assert
-        expect(mockClickCB).toBeCalled();
-        expect(mockClickCB).toHaveBeenCalledTimes(1);
-    });
-
-    it('should minimise the hero component when the button is clicked', async () => {
-        // Arrange
-        const user = userEvent.setup();
-        const mockClickCB = vi.fn();
-
-        // Act
-        render(<Hero clickCB={mockClickCB} />);
-
-        await user.click(screen.getByRole('button', { name: 'Build Your Encounter' }));
-
-        // Assert
-        expect(
-            screen.queryByRole('button', { name: 'Build Your Encounter' }),
-        ).not.toBeInTheDocument();
-        expect(screen.queryByText(/Take the stress out of being a DM/i)).not.toBeInTheDocument();
-        expect(
-            screen.queryByText(/Generate an encounter customised to YOUR party/i),
-        ).not.toBeInTheDocument();
     });
 });
