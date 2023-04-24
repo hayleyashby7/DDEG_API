c09ee6a7dbcb634163c1159bbf6bf2d2bb202934
Hayley Ashby <hayleyashby7@gmail.com>
2023-04-24 10:22:53 +0100

    intial file structure changes to incorporate server

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     content: {
         relative: true,
-        files: ['./index.html', './src/**/*.{js,jsx}', './src/*.{js,jsx}'],
+        files: ['./index.html', '././src/**/*.{js,jsx}', '././src/*.{js,jsx}'],
     },
     theme: {
         extend: {
