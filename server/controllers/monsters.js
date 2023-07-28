50c83acaddeb83cdff6eb6d82696e87ba97f134b
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-28 16:54:39 +0100

    feat: :art: refine stats section of query to relevant values

--- server/controllers/monsters.js
+++ server/controllers/monsters.js
@@ -15,7 +15,14 @@ export default {
                         monsters_senses: {
                             select: { senses: { select: { sense: true } }, value: true },
                         },
-                        monster_stats: true,
+                        monster_stats: {
+                            select: {
+                                stats: { select: { stat: true } },
+                                score: true,
+                                modifier: true,
+                                saving_throw: true,
+                            },
+                        },
                     },
                 });
                 return res.status(200).json(monsters);
